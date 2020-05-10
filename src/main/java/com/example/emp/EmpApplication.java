17b0385545e0f443f6c01202fe11f1cf30c2b313
dorrero <saivamshi.samala@gmail.com>
2020-05-10 20:27:16 +0530

    Added example employee data in resources and code to load that data into JSON object

--- src/main/java/com/example/emp/EmpApplication.java
+++ src/main/java/com/example/emp/EmpApplication.java
@@ -1,13 +1,28 @@
 package com.example.emp;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmpApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ParseException {
+				
+		JSONParser parser = new JSONParser();
+		
+		FileReader reader = new FileReader("/home/sunny/Mastercard/emp/src/main/resources/EmployeeData.json");
+		
+		Object obj = parser.parse(reader);
+		JSONObject empjsonobj = (JSONObject)obj;
+		
 		SpringApplication.run(EmpApplication.class, args);
 	}
-
 }
